ee9f7b3dc2d0ef80a886406ee1b6db3109853c38
Arkadiusz Krzymienski <a.krzymienski@gmail.com>
2021-05-04 14:34:49 +0200

    simply function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './css/App.min.css';
 function App() {
   const [TopPanel, setTopPanel] = useState('');
   const [bottomPanel, setBottomPanel] = useState('');
-  const [operator, setOperator] = useState([]);
   const [number, setNumber] = useState([])
   const [numClick, setNumClick] = useState('')
 
@@ -14,31 +13,27 @@ function App() {
     <div className="App">
       <ResultPanel topPanel={TopPanel} bottomPanel={bottomPanel}/>
       <div className='delete'>
-          <Button nameBtn='clear' setTopPanel={setTopPanel}  setBottomPanel={setBottomPanel} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-          <Button nameBtn='C' setTopPanel={setTopPanel}  setBottomPanel={setBottomPanel} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-          <Button nameBtn='CE' setTopPanel={setTopPanel}  setBottomPanel={setBottomPanel} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
+          <Button nameBtn='clear' setTopPanel={setTopPanel}  setBottomPanel={setBottomPanel} setNumber={setNumber} setNumClick={setNumClick} />
       </div> 
       <div className='numbers'>
-        <Button nameBtn={7} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={8} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={9} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={4} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={5} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={6} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={1} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={2} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={3} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn={0} setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn='.' setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn='=' setTopPanel={setTopPanel} setBottomPanel={setBottomPanel} topPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick} number={number} operator={operator}/>
+        <Button nameBtn={7} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={8} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={9} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={4} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={5} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={6} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={1} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={2} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={3} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn={0} setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn='.' setNumClick={setNumClick} setTopPanel={setTopPanel} type={'number'}/>
+        <Button nameBtn='=' setBottomPanel={setBottomPanel} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick} number={number} />
       </div>
       <div className='operation'>
-      <Button nameBtn='[' setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-      <Button nameBtn=']' setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn='/' setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn='*' setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn='-' setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
-        <Button nameBtn='+' setTopPanel={setTopPanel} setOperator={setOperator} setNumber={setNumber} setNumClick={setNumClick} numClick={numClick}/>
+        <Button nameBtn='/' setTopPanel={setTopPanel}  setNumber={setNumber} setNumClick={setNumClick} numClick={numClick} type={'operation'}/>
+        <Button nameBtn='*' setTopPanel={setTopPanel}  setNumber={setNumber} setNumClick={setNumClick} numClick={numClick} type={'operation'}/>
+        <Button nameBtn='-' setTopPanel={setTopPanel}  setNumber={setNumber} setNumClick={setNumClick} numClick={numClick} type={'operation'}/>
+        <Button nameBtn='+' setTopPanel={setTopPanel}  setNumber={setNumber} setNumClick={setNumClick} numClick={numClick} type={'operation'}/>
       </div>
     </div>
   );
--- src/Button.js
+++ src/Button.js
@@ -1,93 +1,95 @@
 import React from 'react';
 
-const Button = ({nameBtn, setTopPanel, setBottomPanel, setNumClick, setNumber, numClick, number}) => {
+const Button = ({nameBtn, setTopPanel, setBottomPanel, setNumClick, setNumber, numClick, number, type}) => {
     const handleClick = (e) => {
     const target = e.target.innerHTML;
+    const type = e.target.dataset.type;
 
-    setTopPanel(prev => prev + target)
-
-        if(target === '0' || target === '1' || target === '2' || target === '3' || target === '4' || target === '5' || target === '6' || target === '7' || target === '8' || target === '9' || target === '.'){
+    
+        if(type === 'number'){
             setNumClick(prev => prev + target);
-        }else if(target === '/' || target === '*' || target === '-' || target === '+'){
-            setNumber(prev => [...prev, numClick])
-            setNumber(prev => [...prev, target])
-            setNumClick('')
+            setTopPanel(prev => prev + target);
+        }else if(type === 'operation'){
+            setNumber(prev => [...prev, numClick]);
+            setNumber(prev => [...prev, target]);
+            setNumClick('');
+            setTopPanel(prev => prev + target);
         }else if(target === '='){
-            let num = [...number, numClick]
+            let num = [...number, numClick];
             
-            let multiDeviIndex = [...setIndexOperator(num, '*'), ...setIndexOperator(num, '/')]
-            multiDeviIndex.sort((a,b) => a.index - b.index)
+            let multiDeviIndex = [...setIndexOperator(num, '*'), ...setIndexOperator(num, '/')];
+            multiDeviIndex.sort((a,b) => a.index - b.index);
             
             if(multiDeviIndex.length > 0){
                 let j = 0;
                 multiDeviIndex.forEach(index => {
                     if(index.operator === '*'){
-                        num[index.index +  1 - j] = multiply(num[index.index - 1 - j], num[index.index + 1 - j])
-                        num.splice(index.index - 1 - j, 2)
-                        j += 2
+                        num[index.index +  1 - j] = multiply(num[index.index - 1 - j], num[index.index + 1 - j]);
+                        num.splice(index.index - 1 - j, 2);
+                        j += 2;
                     }else{
-                        num[index.index +  1 - j] = division(num[index.index - 1 - j], num[index.index + 1 - j])
-                        num.splice(index.index - 1 - j, 2)
-                        j += 2
+                        num[index.index +  1 - j] = division(num[index.index - 1 - j], num[index.index + 1 - j]);
+                        num.splice(index.index - 1 - j, 2);
+                        j += 2;
                     }
                 })
             }
 
-            let plusMinusIndex = [...setIndexOperator(num, '+'), ...setIndexOperator(num, '-')]
-            plusMinusIndex.sort((a,b) => a.index - b.index)
+            let plusMinusIndex = [...setIndexOperator(num, '+'), ...setIndexOperator(num, '-')];
+            plusMinusIndex.sort((a,b) => a.index - b.index);
             
             if(plusMinusIndex.length > 0){
                 let j = 0;
                 plusMinusIndex.forEach(index => {
                     if(index.operator === '+'){
-                        num[index.index +  1 - j] = plus(num[index.index - 1 - j], num[index.index + 1 - j])
-                        num.splice(index.index - 1 - j, 2)
-                        j += 2
+                        num[index.index +  1 - j] = plus(num[index.index - 1 - j], num[index.index + 1 - j]);
+                        num.splice(index.index - 1 - j, 2);
+                        j += 2;
                     }else{
                         if(num[index.index - 1] === ''){
-                            num[index.index - 1] = 0
+                            num[index.index - 1] = 0;
                         }
-                        num[index.index +  1 - j] = minus(num[index.index - 1 - j], num[index.index + 1 - j])
-                        num.splice(index.index - 1 - j, 2)
-                        j += 2
+                        num[index.index +  1 - j] = minus(num[index.index - 1 - j], num[index.index + 1 - j]);
+                        num.splice(index.index - 1 - j, 2);
+                        j += 2;
                     }
                 })
             }
-            setBottomPanel(num)
+            setBottomPanel(num);
         }
         else if(target === 'clear'){
-            setNumber([])
-            setNumClick('')
-            setTopPanel('')
-            setBottomPanel('')
+            setNumber([]);
+            setNumClick('');
+            setTopPanel('');
+            setBottomPanel('');
         }
     }
 
     const setIndexOperator = (num, operator) => {
-        let indexes = [], i = -1
+        let indexes = [], i = -1;
         while((i = num.indexOf(operator, i+1)) !== -1){
-            indexes.push({index: i, operator:operator})
+            indexes.push({index: i, operator:operator});
         }
-    return indexes
+    return indexes;
     }
 
     const multiply = (x, y) => {
-        return (x*10 * y*10)/100
+        return (x*10 * y*10)/100;
     }
 
     const division = (x, y) => {
-        return x / y
+        return x / y;
     }
     const plus = (x, y) => {
-        return parseFloat(x) + parseFloat(y)
+        return parseFloat(x) + parseFloat(y);
     }
 
     const minus = (x, y) => {
-        return parseFloat(x) - parseFloat(y)
+        return parseFloat(x) - parseFloat(y);
     }
 
     return (
-        <button onClick={handleClick}>{nameBtn}</button>
+        <button onClick={handleClick} data-type={type}>{nameBtn}</button>
     );
     
 } 
